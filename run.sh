370f860b601f5469217ede9035176f01861aab5a
henriquebh8 <henriquenovaes2011@gmail.com>
2024-03-29 12:40:47 -0300

    run script

--- run.sh
+++ run.sh
@@ -12,15 +12,29 @@ check_status() {
 
 echo "Iniciando docker-compose service..."
 
+# Armazena o diretório atual antes de mudar
+original_dir=$(pwd)
 script_dir=$(dirname "$0")
+
+# Mudança para o diretório do serviço e iniciação do docker-compose
 cd "$script_dir/projeto-copybase"
 docker-compose up -d
 docker-compose logs
 check_status
 
+# Volta para o diretório original antes de iniciar o processo do front-end
+cd "$original_dir"
+
 echo "Iniciando docker-compose front-end..."
-cd "$script_dir/projeto-copybase-frontend"
-docker-compose up -d
-check_status
+
+# Verifica se o diretório do front-end existe antes de tentar mudar para ele
+if [ -d "$script_dir/projeto-copybase-frontend" ]; then
+  cd "$script_dir/projeto-copybase-frontend"
+  docker-compose up -d
+  check_status
+else
+  echo "Erro: Diretório $script_dir/projeto-copybase-frontend não encontrado."
+  exit 1
+fi
 
 echo "Ambos os containers foram iniciados com sucesso."
